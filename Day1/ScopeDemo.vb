c52267dd7dbd0c01009f2b96428e2a5f49cbf872
kneerunjun <kneerunjun@outlook.com>
2015-12-14 12:22:10 +0530

    logic and scope demo

---
+++ Day1/ScopeDemo.vb
@@ -0,0 +1,16 @@
+﻿Module ScopeDemo
+    ' module level variable
+    Dim number As Integer
+
+    Function DoThisTask() As Integer
+        'local variable
+        Dim age As Integer = 20
+        number = 20
+        Return 0
+    End Function
+
+    Function DoThisAsWell() As Boolean
+        number = 30
+        Return True
+    End Function
+End Module
