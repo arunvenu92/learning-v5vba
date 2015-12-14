a5954846fcb562ac468ade53a7fdc5e7a086d867
kneerunjun <kneerunjun@outlook.com>
2015-12-14 10:27:20 +0530

    operationsDemo

--- Day1/Module1.vb
+++ Day1/Module1.vb
@@ -51,6 +51,56 @@
         Console.ReadLine()
 
 
+        'integer operations
+        Dim num2 As Integer = 20
+        Dim num3 As Integer = 30
+
+        'addition
+        Dim addResult As Integer
+        addResult = num2 + num3
+        Console.WriteLine("{0} number 1 plus {1} number 2 is equal to Addresult {2}", num2, num3, addResult)
+        Console.ReadLine()
+
+
+        'subtraction
+        Dim subResult As Integer
+        subResult = num3 - num2
+
+        'product
+        Dim prodResult As Integer
+        prodResult = num2 * num3
+
+        'division
+        Dim divResult As Integer
+        divResult = num3 / num2
+
+        Dim quotient As Integer
+        Dim remainder As Integer
+        quotient = Math.DivRem(num2, num3, remainder)
+        Console.WriteLine(quotient)
+
+        'string operations
+        Dim str1 As String
+        Dim str2 As String
+
+        str1 = "This is Day 1 of VB session"
+        str2 = " and we are confident of learning VB or atleast are hopeful!"
+
+        Dim strResult As String
+        'concatenation
+        strResult = str1 + str2 ' this is not recommended
+
+        strResult = str1 & str2 ' use & for concatenation
+        Console.WriteLine(strResult)
+        Console.ReadLine()
+
+        'position of string within a string
+        Dim position As Integer
+        position = InStr(strResult, "VB")
+        Console.WriteLine(position)
+        Console.ReadLine()
+
+
     End Sub
 
 End Module
